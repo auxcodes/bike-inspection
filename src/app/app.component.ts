af17f8bc8bc050799edbc6d9c889a27f46a5c613
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2021-07-12 22:11:21 +1000

    Add output feild and test input text is displayed in output.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bike-inspection';
+  output = '';
 
   onFieldChange(field: any) {
     console.log("field: ", field.value);
-    ;
+    this.output = field.value;
   }
 }
