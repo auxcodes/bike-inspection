9aa4aa555e03892fa765ce58d76f781c68f1acbb
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2021-07-13 22:37:10 +1000

    Add dynamic field generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonField } from './shared/interfaces/json-field';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bike-inspection';
   output = '';
+
+  brakeFields: JsonField[] = [
+    { name: 'pads', text: 'Pads', selected: false, id: '$pads', value: 'pads' },
+    { name: 'lever', text: 'Lever Feel', selected: false, id: '$lever', value: 'lever' },
+    { name: 'bleed', text: 'Bleed', selected: false, id: '$bleed', value: 'bleed' }
+  ]
 
   onFieldChange(field: any) {
     console.log("field: ", field.value);
---
+++ src/app/shared/interfaces/json-field.ts
@@ -0,0 +1,8 @@
+export interface JsonField {
+  name: string;
+  text: string;
+  selected: boolean;
+  id: string;
+  value?: any;
+  error?: string;
+}
