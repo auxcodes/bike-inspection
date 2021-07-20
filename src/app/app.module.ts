32dae8c7e1e1c8f061b47046c8467c9dd19cf0ce
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2021-07-20 22:23:42 +1000

    Refactor app component into seperate componets . Add styling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BodyComponent } from './pages/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
