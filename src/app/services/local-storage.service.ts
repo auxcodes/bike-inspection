f2ffd8e057899f1bb9d328ffb4f812cc5dc77080
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2021-08-01 21:51:08 +1000

    Add local storage service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  supported = true;
+  hasEntry = () => localStorage.length > 0;
+
+  constructor() {
+    this.supported = window.localStorage ? true : false;
+  }
+
+
+  updateEntry(key: string, value: string) {
+    if (this.supported) {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  updateJSONEntry(key: string, value: string) {
+    if (this.supported) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }
+
+  readEntry(key: string) {
+    if (this.supported && this.hasEntry) {
+      return localStorage.getItem(key);
+    }
+    return null;
+  }
+
+  async readJSONEntry(key: string): Promise<object> {
+    if (this.supported && this.hasEntry) {
+      return JSON.parse(localStorage.getItem(key));
+    }
+    else {
+      return null;
+    }
+  }
+
+  deleteEntry(key: string) {
+    if (this.supported && this.hasEntry) {
+      localStorage.removeItem(key);
+    }
+  }
+
+  clearStorage() {
+    if (this.supported && this.hasEntry) {
+      localStorage.clear();
+    }
+  }
+}
