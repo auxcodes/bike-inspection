8d1ccab5e3613af414e9f82de8ce90c70020d244
DESKTOP-VB5GUSP\Phill <borgosity@gmail.com>
2021-10-27 22:06:11 +1100

    Editor: display fields, add toggle edit on fields

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldsService } from '../../services/fields.service';
+import { JsonField } from '../../shared/interfaces/json-field';
+import { JsonGroup } from '../../shared/interfaces/json-group';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+  fieldGroups: JsonGroup[] = [];
+  fields: JsonField[] = [];
+
+  constructor(private fieldService: FieldsService) { }
 
   ngOnInit(): void {
+    this.fieldService.fields.subscribe(fields => {
+      this.fieldGroups = fields;
+    });
+  }
+
+  onAddField() {
+
+  }
+
+  onAddGroup() {
+
+  }
+
+  onRemoveField() {
+
+  }
+
+  onRemoveGroup() {
+
+  }
+
+  onToggleExtraNotes() {
+
+  }
+
+  onSaveChanges() {
+    // backup last version
+      // check history to see if there is any reference to version
+
+    // save new version locally and in the cloud
+
   }
 
 }
